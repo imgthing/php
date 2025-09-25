a64004b56b5f19b9d7afc04a3f0169b8316cd916
Oleg Sedinkin <a@aw.wtf>
2025-09-25 20:03:22 +0500

    fix plain source

--- src/Source/PlainSource.php
+++ src/Source/PlainSource.php
@@ -7,6 +7,6 @@ class PlainSource extends AbstractSource
     public function __construct(
         string $path,
     ) {
-        $this->encoded = urlencode($path);
+        $this->encoded = sprintf('plain/%s', urlencode($path));
     }
 }
